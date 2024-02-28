74f292fc6e3e8e62140ab1070a8ed9b2b3e80904
DOVENE AGOGUE <adovene@gmail.com>
2024-02-28 14:41:30 +0100

    first commit

---
+++ src/main/java/com/dov/App.java
@@ -0,0 +1,13 @@
+package com.dov;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
